46baec08d0fdfc963c14be325dc6f199087cb899
unknown <valerde88@outlook.com>
2026-01-08 20:00:55 -0300

    Fionalização de projeto de keys

--- src/components/BatButton/BatButton.tsx
+++ src/components/BatButton/BatButton.tsx
@@ -1,43 +1,32 @@
-import React, { useState } from 'react';
-import { TouchableOpacity, Text, View } from 'react-native';
-import { styles } from './BatButtonStyle';
-import { BattextInput } from '../BattextInput/BatTextInput';
-import generatePass from '../../service/passwordService';
-
-
-
+import React, { useState } from "react";
+import { TouchableOpacity, Text, View } from "react-native";
+import { styles } from "./BatButtonStyle";
+import { BattextInput } from "../BattextInput/BatTextInput";
+import generatePass from "../../service/passwordService";
+import * as Clipboard from "expo-clipboard";
 
 export function BatButton() {
+  const [pass, setPass] = useState<string>("");
 
-const [pass, setPass] = useState<string>('')
+  function handleGnerateButton() {
+    let generateToken = generatePass();
 
-function handleGnerateButton(){
-  let generateToken = generatePass()
+    setPass(generateToken);
+  }
 
-  setPass(generateToken)
-}
+  function handleCopy() {
+    Clipboard.setStringAsync(pass);
+  }
 
   return (
-
     <>
-
-      <BattextInput
-      pass={pass}
-      />
-        <TouchableOpacity
-        style={styles.ButtonCopy}
-        onPress={handleGnerateButton}>
+      <BattextInput pass={pass} />
+      <TouchableOpacity style={styles.ButtonCopy} onPress={handleGnerateButton}>
         <Text style={styles.text}>⌛ GENERATE </Text>
       </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.ButtonCopy}
-          onPress={()=>{
-
-          }}>
-          <Text style={styles.text}>💾 COPY </Text>
+      <TouchableOpacity style={styles.ButtonCopy} onPress={handleCopy}>
+        <Text style={styles.text}>💾 COPY </Text>
       </TouchableOpacity>
-
     </>
-    
   );
 }
--- src/components/BattextInput/BatTextInputStyle.tsx
+++ src/components/BattextInput/BatTextInputStyle.tsx
@@ -1,16 +1,16 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   inputer: {
-    backgroundColor:'#e5bf3c',
-    width:'100%',
-    color:'#000',
-    fontSize:20,
-    lineHeight:22,
-    height:40,
-    borderWidth:1,
-    borderRadius:5,
-    padding:10,
-    textAlign:'center'
-  }
+    backgroundColor: "#e5bf3c",
+    width: "100%",
+    color: "#000",
+    fontSize: 20,
+    lineHeight: 24,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    textAlign: "center",
+  },
 });
